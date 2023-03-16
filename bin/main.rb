6b61163699612e18fba3483a9ba35250f3474220
Nkuria <nkuria01@gmail.com>
2023-03-16 12:04:51 +0300

    Finished working on tic tac toe game

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,13 @@
-puts "Enter player 1 name: "
-player_1_name = gets
-puts "Enter player 2 name: "
-player_2_name = gets
+# puts "Enter player 1 name: "
+# player_1_name = gets
+# puts "Enter player 2 name: "
+# player_2_name = gets
 
-player_1 = Player.new(player_1_name)
-player_2 = Player.new(player_2_name)
+# player_1 = Player.new(player_1_name)
+# player_2 = Player.new(player_2_name)
+require_relative "../lib/game.rb"
+
+  
+  game = Game.new
+  game.play
+
